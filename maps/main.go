efe7d6309cc53f800a00eb02c09bef8b778f17a0
Gnaneshwara Babu <24668767+code2gnana@users.noreply.github.com>
2022-07-20 22:52:51 +1000

    Adding Maps (#2)

---
+++ maps/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Another way of declaring maps
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	//	colors["White"] = "#ffffff"
+
+	//	delete(colors, "White")
+
+	//	fmt.Println(colors)
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
