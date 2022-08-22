62de787686a1cf7e531d45291e0a511a6a18536b
Gnaneshwara Babu <24668767+code2gnana@users.noreply.github.com>
2022-08-22 22:47:54 +1000

    adding tests for area.go

---
+++ shapes/area_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestArea(t *testing.T) {
+	s := square{sideLength: 20}
+	t := triangle{base: 10, height: 20}
+
+	got := getArea(s)
+	want := 400
+
+	t.Errorf("got %q, wanted %q", got, want)
+
+}
